847146deb8fa99906cb9095ae0effb1b10f1738e
Joel <52170956+j-a-13@users.noreply.github.com>
2021-03-21 13:30:34 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,91 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const crypto = require("crypto");
+
+var accept = true
+var connections = new Array()
+
+function escape_html(str) { 
+  var map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#039;"
+  };
+  
+  var regex = /[&<>"']/g;
+  
+  str = str.replace(regex, function (char) {return map[char]; });
+  return str;
+}
+
+
+http.createServer(function (req, res) {
+	var q = url.parse(req.url, true);
+	//console.log(q.pathname);
+	if (q.pathname == "/liveUpdates") {
+		if (accept) {
+			connections.push([res]);
+			//console.log(connections);
+			res.writeHead(200, {"pragma": "no-cache",
+				"Content-Type": "application/json"});
+		} else {
+			res.writeHead(403);
+			res.end();
+		}
+	} else if (q.pathname == "/message") {
+		var len  = connections.length;
+		accept = false;
+		for (i=0; i<len; i++) {
+			connections[0][0].write('{"message": "' + escape_html(decodeURIComponent(q.query.message)) + '","name": "' + escape_html(decodeURIComponent(q.query.name)) + '"}');
+			connections[0][0].end();
+			connections.shift();
+			//console.log(connections);
+		}
+		accept = true;
+		//connections = new Array();
+		res.writeHead(200);
+		res.end();
+	} else if (q.pathname == "/") {
+		var filename = "./files/html/index.html";
+		/*fs.readFile(filename, function (err, data) { 
+			if (err) {
+				res.writeHead(404, {"Content-Type": "text/html"});
+				return res.end("Error 404. File not found.");
+			} else {
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.write(data);
+				return res.end();
+			}
+		});*/
+    
+    res.writeHeader(301, {"Location": "/v2"});
+    res.end();
+	} else if (q.pathname == "/v2") {
+    var filename = "./files/html/index-v2.html";
+		fs.readFile(filename, function (err, data) { 
+			if (err) {
+				res.writeHead(404, {"Content-Type": "text/html"});
+				return res.end("Error 404. File not found.");
+			} else {
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.write(data);
+				return res.end();
+			}
+		});
+  }  else {
+		var filename = "./files" + q.pathname;
+		fs.readFile(filename, function (err, data) { 
+			if (err) {
+				res.writeHead(404, {"Content-Type": "text/html"});
+				return res.end("Error 404. File not found.");
+			} else {
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.write(data);
+				return res.end();
+			}
+		});
+	}
+}).listen(3000);
